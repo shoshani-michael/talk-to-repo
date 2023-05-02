ddf5b7d48a731c5aecef69c07450a4a6ed30d15d
Jonathan S. Rouach <jon@arjeo.com>
2023-05-02 13:13:42 +0300

    Add loading/success/error next to Load Repo button

--- components/GitHubInput.js
+++ components/GitHubInput.js
@@ -4,18 +4,29 @@ function GitHubInput() {
   const [username, setUsername] = useState("");
   const [repo, setRepo] = useState("");
   const [token, setToken] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [loadingStatus, setLoadingStatus] = useState(null);
 
   const handleClick = async () => {
+    setLoading(true); // Start loading
+    setLoadingStatus(null);
+    
     try {
       const response = await fetch(process.env.NEXT_PUBLIC_BACKEND_URL + "/load_repo", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, repo }),
-    });
+      });
+
       const data = await response.json();
       console.log(data);
+      
+      setLoading(false); // Stop loading
+      setLoadingStatus(response.ok ? 'success' : 'error');
     } catch (error) {
       console.error(error);
+      setLoading(false); // Stop loading
+      setLoadingStatus('error');
     }
   };
 
@@ -48,10 +59,12 @@ function GitHubInput() {
         onChange={(e) => setToken(e.target.value)}
       />
       <br />
-      <button onClick={handleClick} 
-        className="ml-2 px-2 py-1 rounded-lg bg-blue-500 text-white focus:outline-none hover:bg-blue-600 md:ml-4 md:px-4 md:py-2">
+      <button onClick={handleClick} className="ml-2 px-2 py-1 rounded-lg bg-blue-500 text-white focus:outline-none hover:bg-blue-600 md:ml-4 md:px-4 md:py-2">
         Load Repo
       </button>
+      {loading && <span className="ml-2 animate-spin">&#9203;</span>}
+      {!loading && loadingStatus === 'success' && <span className="ml-2 text-green-500">&#10004;</span>}
+      {!loading && loadingStatus === 'error' && <span className="ml-2 text-red-500">&#10006;</span>}
     </div>
   );
 }
