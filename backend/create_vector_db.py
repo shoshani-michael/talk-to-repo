d76dd54963984c13b19b458a642f86a7f9af9385
Jonathan S. Rouach <jon@arjeo.com>
2023-05-04 00:07:52 +0300

    Switch to git clone instead of downloading zip

--- backend/create_vector_db.py
+++ backend/create_vector_db.py
@@ -2,18 +2,16 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Pinecone
 import pinecone
-from urllib.request import Request
 
 import pandas as pd
 import tiktoken
 
-from tqdm import tqdm
 
 import os
 import re
-import zipfile
 from urllib.request import urlopen
-from io import BytesIO
+import tempfile
+import subprocess
 
 # Import dotenv and load the variables from .env file
 from dotenv import load_dotenv
@@ -41,24 +39,16 @@ splitter = RecursiveCharacterTextSplitter(
     chunk_overlap=int(os.environ['CHUNK_OVERLAP'])
     )
 
-def download_from_github():
-    req = Request(os.environ['ZIP_URL'])
-    token = os.environ['GITHUB_TOKEN'] if 'GITHUB_TOKEN' in os.environ else None
-    print(token, os.environ['ZIP_URL'])
+def clone_from_github():
+    temp_dir = tempfile.mkdtemp()
+    repo_url = os.environ["REPO_URL"]
+    token = os.environ["GITHUB_TOKEN"] if "GITHUB_TOKEN" in os.environ else None
+
     if token:
-        req.add_header("Authorization", f"Bearer {token}")
+        repo_url = repo_url.replace("https://", f"https://x-access-token:{token}@")
 
-    http_response = urlopen(req)
-
-    # Save the zip file to filesystem
-    with open('data/downloaded_zip.zip', 'wb') as f:
-        f.write(http_response.read())
-    
-    # Return the zipfile object using the file saved on the filesystem
-    zip_ref = zipfile.ZipFile('data/downloaded_zip.zip', 'r')
-    file_list = zip_ref.namelist()
-    filtered_file_list = [file_name for file_name in file_list if not is_unwanted_file(file_name)]
-    return filtered_file_list, zip_ref
+    subprocess.run(["git", "clone", repo_url, temp_dir], check=True)
+    return temp_dir
 
 def is_unwanted_file(file_name):
     if (file_name.endswith('/') or 
@@ -73,17 +63,19 @@ def process_file(file):
     n_tokens = len(encoder.encode(file_contents))
     return file_contents, n_tokens
 
-def process_file_list(file_list, zip_ref):
+def process_file_list(temp_dir):
     corpus_summary = []
     file_texts, metadatas = [], []
 
-    for file_name in file_list:
-        with zip_ref.open(file_name, 'r') as file:
-            file_contents, n_tokens = process_file(file)
-            file_name_trunc = re.sub(r'^[^/]+/', '', str(file_name))
-            corpus_summary.append({'file_name': file_name_trunc, 'n_tokens': n_tokens})
-            file_texts.append(file_contents)
-            metadatas.append({'document_id': file_name_trunc})
+    for root, _, files in os.walk(temp_dir):
+        for filename in files:
+            if not is_unwanted_file(filename):
+                file_path = os.path.join(root, filename)
+                file_contents, n_tokens = process_file(file_path)
+                file_name_trunc = re.sub(r'^[^/]+/', '', str(filename))
+                corpus_summary.append({'file_name': file_name_trunc, 'n_tokens': n_tokens})
+                file_texts.append(file_contents)
+                metadatas.append({'document_id': file_name_trunc})
 
     split_documents = splitter.create_documents(file_texts, metadatas=metadatas)
     vector_store.from_documents(
@@ -97,5 +89,5 @@ def process_file_list(file_list, zip_ref):
 
     return 
 
-filtered_file_list, zip_ref = download_from_github()
-process_file_list(filtered_file_list, zip_ref)
+temp_dir = clone_from_github()
+process_file_list(temp_dir)
