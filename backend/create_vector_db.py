099299c1409fd34bd8a7d7a33d1ca724ab85ee1f
Jonathan S. Rouach <jon@arjeo.com>
2023-05-03 23:27:55 +0300

    Refactor create_vector_db

--- backend/create_vector_db.py
+++ backend/create_vector_db.py
@@ -19,22 +19,6 @@ from io import BytesIO
 from dotenv import load_dotenv
 load_dotenv()
 
-def zipfile_from_github():
-    req = Request(os.environ['ZIP_URL'])
-    token = os.environ['GITHUB_TOKEN'] if 'GITHUB_TOKEN' in os.environ else None
-    print(token, os.environ['ZIP_URL'])
-    if token:
-        req.add_header("Authorization", f"Bearer {token}")
-
-    http_response = urlopen(req)
-
-    # Save the zip file to filesystem
-    with open('data/downloaded_zip.zip', 'wb') as f:
-        f.write(http_response.read())
-    
-    # Return the zipfile object using the file saved on the filesystem
-    return zipfile.ZipFile('data/downloaded_zip.zip', 'r')
-
 embeddings = OpenAIEmbeddings(
     openai_api_key=os.environ['OPENAI_API_KEY'],
     openai_organization=os.environ['OPENAI_ORG_ID'],
@@ -57,27 +41,45 @@ splitter = RecursiveCharacterTextSplitter(
     chunk_overlap=int(os.environ['CHUNK_OVERLAP'])
     )
 
-def process_zip_files(zip_ref):
-    total_tokens, corpus_summary = 0, []
+def zipfile_from_github():
+    req = Request(os.environ['ZIP_URL'])
+    token = os.environ['GITHUB_TOKEN'] if 'GITHUB_TOKEN' in os.environ else None
+    print(token, os.environ['ZIP_URL'])
+    if token:
+        req.add_header("Authorization", f"Bearer {token}")
+
+    http_response = urlopen(req)
+
+    # Save the zip file to filesystem
+    with open('data/downloaded_zip.zip', 'wb') as f:
+        f.write(http_response.read())
+    
+    # Return the zipfile object using the file saved on the filesystem
+    return zipfile.ZipFile('data/downloaded_zip.zip', 'r')
+
+def is_unwanted_file(file_name):
+    if (file_name.endswith('/') or 
+        any(f in file_name for f in ['.DS_Store', '.gitignore']) or 
+        any(file_name.endswith(ext) for ext in ['.png', '.jpg', '.jpeg', '.mp3'])
+    ):
+        return True
+    return False
+
+def process_file(file):
+    file_contents = str(file.read())
+    n_tokens = len(encoder.encode(file_contents))
+    return file_contents, n_tokens
+
+def process_file_list(file_list):
+    corpus_summary = []
     file_texts, metadatas = [], []
 
-    zip_file_list = zip_ref.namelist()
-
-    for file_name in zip_file_list:
-        if (file_name.endswith('/') or 
-            any(f in file_name for f in ['.DS_Store', '.gitignore']) or 
-            any(file_name.endswith(ext) for ext in ['.png', '.jpg', '.jpeg', '.mp3'])
-        ):
-            continue
-        else:
+    for file_name in file_list:
+        if not is_unwanted_file(file_name):
             with zip_ref.open(file_name, 'r') as file:
-                file_contents = str(file.read())
+                file_contents, n_tokens = process_file(file)
                 file_name_trunc = re.sub(r'^[^/]+/', '', str(file_name))
-
-                n_tokens = len(encoder.encode(file_contents))
-                total_tokens += n_tokens
                 corpus_summary.append({'file_name': file_name_trunc, 'n_tokens': n_tokens})
-
                 file_texts.append(file_contents)
                 metadatas.append({'document_id': file_name_trunc})
 
@@ -91,7 +93,8 @@ def process_zip_files(zip_ref):
 
     pd.DataFrame.from_records(corpus_summary).to_csv('data/corpus_summary.csv', index=False)
 
-    return total_tokens, corpus_summary
+    return 
 
 with zipfile_from_github() as zip_ref:
-    process_zip_files(zip_ref)
+    zip_file_list = zip_ref.namelist()
+    process_file_list(zip_file_list)
