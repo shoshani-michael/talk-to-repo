55eb6e5b050377981287f0ec049f398e9f259b97
Jonathan S. Rouach <jon@arjeo.com>
2023-05-20 19:25:27 +0300

    File 1:
    - Added new function to calculate total cost
    
    Commit message

--- components/Header.js
+++ components/Header.js
@@ -4,18 +4,16 @@ import GitHubInput from './GitHubInput';
 const Header = (props) => {
   return (
     <div className="bg-gray-700 text-white text-center py-4">
-      <h1 className="text-2xl font-semibold">🚀 Chat With any Repo</h1>
+      <h1 className="text-2xl font-semibold">Talk to Repo</h1>
       <GitHubInput
         importMessages={props.importMessages}
-        clearMessages={props.clearMessages}
-        messages={props.messages}
+        clearMessages={props.clearMessages}        messages={props.messages}
         handleCommitCodeSnippets={props.handleCommitCodeSnippets}
       />
-      <div className="pt-2 text-center text-xs text-gray-400">
+      <div className="pt-2 text-center text-sm text-gray-400">
         <a
           href="https://github.com/twitter/the-algorithm"
-          target="_blank"
-          rel="noopener noreferrer"
+          target="_blank"          rel="noopener noreferrer"
         >
           based on https://github.com/twitter/the-algorithm
         </a>
